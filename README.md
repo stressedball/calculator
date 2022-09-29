c61d65142ce1ee5a33146615cb120cb7f80ae12d
stressedball <tserchesi@gmail.com>
2022-09-29 18:19:23 +0200

    Make display show input
    - Make single input not override chain of numbers displayed.
    - Got CSP warning which pretty much made me redo the whole project. Still can't figure out how to overcome the BIG RED WARNING.
    - Adding every file in case I changed something from last commit.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Mainly consists of buttons.
 Style grid for calculator.
 
 Make JS script to get input and return result.
+
+As of 29/09/2022
+Display shows numbers that are selected by user as a chain of numbers. (Doesn't write over every input).
--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+const operations = document.getElementById('operations');
+operations.addEventListener('click', operate);
+function operate(operator) {
+    
+    switch(operator.target.textContent) {
+        case 'addition':
+            addition();
+            break;
+        case 'substraction':
+            substraction();
+            break;
+        case 'multiplication':
+            multiplication();
+            break;
+        case 'division':
+            division();
+            break;
+    }
+}
+
+const calculatorContainer = document.getElementById('calculatorContainer');
+const calculatorButtons = document.getElementById('calculatorButtons');
+
+
+//DISPLAY-BUTTONS FUNCTIONALITY
+const displayScreen = document.getElementById('display');
+displayScreen.textContent = 'Enter something';
+let numeroUno = 0;
+calculatorContainer.addEventListener('click', display);
+function display(button) {
+    if (button.target !== document.getElementById('hidden') && button.target !== document.getElementById('display')) {
+        let displayedNumber = 0;
+        displayedNumber = storeNumber(button.target.textContent);
+        displayScreen.textContent = displayedNumber;
+        
+    }
+    
+}
+
+//STORING NUMBERS
+let chainNumbersOne = [];
+function storeNumber(inputNumber) {
+    chainNumbersOne += inputNumber;
+    console.log(chainNumbersOne);
+    return chainNumbersOne;
+}
+let chainNumbersTwo = [];
+function storeNumber2(inputNumber) {
+    chainNumbersTwo += inputNumber;
+    console.log(chainNumbersTwo);
+    return chainNumbersTwo;
+}
+//CLEAR BUTTON
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', clearFunction);
+function clearFunction() {
+    displayScreen.textContent = '0';
+}
+
+
+
+
+
+
 function addition() {
     const x = prompt('input number');
     const y = prompt('input number 2');
@@ -24,22 +88,4 @@ function division(){
     const y = prompt('input number 2');
     const z = Number(x) / Number(y);
     return z;    
-}
-
-function operate() {
-    const operator = prompt('enter an operator');
-    switch(operator) {
-        case '+':
-            const additionResult = addition();
-            break;
-        case '-':
-            substraction();
-            break;
-        case '*':
-            multiplication();
-            break;
-        case '/':
-            division();
-            break;
-    }
 }
