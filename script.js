e336623a24e460498dda29f7deb6c28f0d098920
stressedball <tserchesi@gmail.com>
2022-09-29 20:17:14 +0200

    Make clear button work

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
 const operations = document.getElementById('operations');
 operations.addEventListener('click', operate);
 function operate(operator) {
     
-    switch(operator.target.textContent) {
+    switch(operator.target.id) {
         case 'addition':
             addition();
             break;
@@ -16,6 +17,14 @@ function operate(operator) {
             division();
             break;
     }
+    return true;
+}
+
+function addition() {
+    const x = storeNumber();
+    const y = storeNumber2();
+    const z = x + y;
+    return z;
 }
 
 const calculatorContainer = document.getElementById('calculatorContainer');
@@ -25,36 +34,56 @@ const calculatorButtons = document.getElementById('calculatorButtons');
 //DISPLAY-BUTTONS FUNCTIONALITY
 const displayScreen = document.getElementById('display');
 displayScreen.textContent = 'Enter something';
-let numeroUno = 0;
+let outerClear = '';
+
+
 calculatorContainer.addEventListener('click', display);
 function display(button) {
-    if (button.target !== document.getElementById('hidden') && button.target !== document.getElementById('display')) {
+
+     if (button.target !== document.getElementById('hidden') && button.target !== document.getElementById('display')) {
+        
         let displayedNumber = 0;
         displayedNumber = storeNumber(button.target.textContent);
         displayScreen.textContent = displayedNumber;
-        
+
+        // let displayedNumber2 = 0;
+        // displayedNumber2 = storeNumber2(button.target.textContent);
+        // displayScreen.textContent += displayedNumber2;
+
     }
-    
 }
+
 
 //STORING NUMBERS
 let chainNumbersOne = [];
 function storeNumber(inputNumber) {
+    if (outerClear === 'clear') {
+        chainNumbersOne = [];
+        outerClear = '';
+    }
     chainNumbersOne += inputNumber;
-    console.log(chainNumbersOne);
     return chainNumbersOne;
 }
+
 let chainNumbersTwo = [];
 function storeNumber2(inputNumber) {
+    if (outerClear === 'clear') {
+        chainNumbersTwo = [];
+        outerClear = '';
+    }
     chainNumbersTwo += inputNumber;
-    console.log(chainNumbersTwo);
     return chainNumbersTwo;
 }
+
 //CLEAR BUTTON
 const clearButton = document.getElementById('clear');
 clearButton.addEventListener('click', clearFunction);
-function clearFunction() {
-    displayScreen.textContent = '0';
+function clearFunction(target) {
+    if (target) {
+        displayScreen.textContent = '0';
+        outerClear = 'clear';
+        return outerClear;
+    }
 }
 
 
@@ -62,30 +91,25 @@ function clearFunction() {
 
 
 
-function addition() {
-    const x = prompt('input number');
-    const y = prompt('input number 2');
-    const z = Number(x) + Number(y);
-    return z;
-}
 
-function substraction() {
-    const x = prompt('input number');
-    const y = prompt('input number 2');
-    const z = Number(x) - Number(y);
-    return z;   
-}
 
-function multiplication() {
-    const x = prompt('input number');
-    const y = prompt('input number 2');
-    const z = Number(x) * Number(y);
-    return z;    
-}
+// function substraction() {
+//     const x = prompt('input number');
+//     const y = prompt('input number 2');
+//     const z = Number(x) - Number(y);
+//     return z;   
+// }
 
-function division(){
-    const x = prompt('input number');
-    const y = prompt('input number 2');
-    const z = Number(x) / Number(y);
-    return z;    
-}
+// function multiplication() {
+//     const x = prompt('input number');
+//     const y = prompt('input number 2');
+//     const z = Number(x) * Number(y);
+//     return z;    
+// }
+
+// function division(){
+//     const x = prompt('input number');
+//     const y = prompt('input number 2');
+//     const z = Number(x) / Number(y);
+//     return z;    
+// }
