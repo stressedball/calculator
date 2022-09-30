b9a73c5d1eae21faf4d358b649873836211cf4cf
stressedball <tserchesi@gmail.com>
2022-09-30 22:06:10 +0200

    Make display input first number. Make display take operator and show it. Second Number displaying on console, need to find a way to add it to whole display. User can input a simple x operate y and show result on display. Need to add clear functionality, whole operation display, let user reenter inputs (event listeners are removed after operate, need to find a way to loop between add and remove listeners?)

--- script.js
+++ script.js
@@ -1,114 +1,101 @@
 const calculatorContainer = document.getElementById('calculatorContainer');
 const numberButtons = document.querySelector('#numberButtons');
 const operationButtons = document.getElementById('operationButtons');
-let outerClear = 0;
-let outerClear2 = 0;
-let outerClear3 = 0;
 //DISPLAY-BUTTONS FUNCTIONALITY
 const displayScreen = document.getElementById('display');
 displayScreen.textContent = '0';
 
 const operationsArray = ['+', '-', '/', '*'];
-const numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ','];
+const numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 
-let getNumberOne = 0;
-let getNumberTwo = 0;
-calculatorContainer.addEventListener('click', operateSomething);
-function operateSomething(button) {
+let getNumberOne = null;
+let getNumberTwo = null;
+operationButtons.addEventListener('click', operate);
+let operatorReference = null;
+
+ //DISPLAY NUMBER 1 
+ calculatorContainer.addEventListener('click', display);
+ function display(button) {
+     for (let i = 0; i < numbersArray.length; i++) {
+         if (button.target.textContent === numbersArray[i]) {
+             getNumberOne = storeNumber(button.target.textContent);
+             displayScreen.textContent = getNumberOne;
+         }
+     }
+ }
+
+function operate(button) {
+    operatorReference = button.target;
+    let operatorId = operatorReference.id;
+    let operatorText = operatorReference.textContent;
+
     for (let i = 0; i < operationsArray.length; i++) {
         if (button.target.textContent === operationsArray[i]) {
-            outerClear = 1;
             calculatorContainer.removeEventListener('click', display);
-            outerClear2 = 1;
             calculatorContainer.addEventListener('click', display2);
         }
+    }
+    
+    displayScreen.textContent += ' ' + operatorText + ' ';
+    switch(operatorId) {
+        case 'addition':
+            addition(getNumberOne, getNumberTwo);
+            break;
+        case 'substraction':
+            substraction(getNumberOne, getNumberTwo);
+            break;
+        case 'multiplication':
+            multiplication(getNumberOne, getNumberTwo);
+            break;
+        case 'division':
+            division(getNumberOne, getNumberTwo);
+            break;
+    }
+}
+
+function operate2 () {
+    let operatorId = operatorReference.id;
+    switch(operatorId) {
+        case 'addition':
+            return addition(getNumberOne, getNumberTwo);
+        case 'substraction':
+            return substraction(getNumberOne, getNumberTwo);
+        case 'multiplication':
+            return multiplication(getNumberOne, getNumberTwo);
+        case 'division':
+            return division(getNumberOne, getNumberTwo);
     }
 }
 
 function display2(button) {
-    for (let i = 0; i < operationsArray.length; i++) {
-        if (button.target.textContent === operationsArray[i] 
-            || outerClear3 === 1) {
-            return;
-        }
-    }
-
-    for (let i = 0; i < numbersArray.length; i++) {
-        if (button.target.textContent === numbersArray[i] 
-            && outerClear2 === 1) {
-            getNumberTwo = storeNumber2(button.target.textContent);
-            console.log('number 2 in display event listener : ', getNumberTwo)
-        }
-    }
-}
-
-calculatorContainer.addEventListener('click', display);
-function display(button) {
-    for (let i = 0; i < operationsArray.length; i++) {
-        if (button.target.textContent === operationsArray[i]
-            || button.target.id === 'equals') {
-            return;
-        }
-    }
-
+    calculatorContainer.removeEventListener('click', operate);
     for (let i = 0; i < numbersArray.length; i++) {
         if (button.target.textContent === numbersArray[i]) {
-            getNumberOne = storeNumber(button.target.textContent);
-            console.log(getNumberOne);
+            getNumberTwo = storeNumber2(button.target.textContent);
+            console.log('Second number = ', getNumberTwo);
         }
     }
 }
 
-
-// function operate(operator) {
-//     switch(operator) {
-//         case 'addition':
-//             addition();
-//             break;
-//         case 'substraction':
-//             substraction();
-//             break;
-//         case 'multiplication':
-//             multiplication();
-//             break;
-//         case 'division':
-//             division();
-//             break;
-//     }
-// }
-
-function addition(x, y) {
-    const z = Number(x) + Number(y);
-    return z;
-}
 
 calculatorContainer.addEventListener('click', result);
 function result(button) {
     if (button.target === document.getElementById('equals')) {
         calculatorContainer.removeEventListener('click', display2); 
-        outerClear3 = 3;
-        console.log('number 2 in result function : ', getNumberTwo);
-        console.log('result is : ', addition(getNumberOne, getNumberTwo));
+        displayScreen.textContent = `Result is : ${operate2(getNumberOne, getNumberTwo)}`;
     }
 }
 
 //STORING NUMBERS
 let chainNumbersOne = [];
 function storeNumber(inputNumber) {
-    if (outerClear === 1) {
-        return chainNumbersOne;
-    }
     chainNumbersOne += inputNumber;
     return chainNumbersOne;
 }
 
 let chainNumbersTwo = [];
 function storeNumber2(inputNumber) {
-    if (outerClear3 === 1) {
-        return chainNumbersTwo;
-    } else if (outerClear2 === 1) {
-        chainNumbersTwo += inputNumber;
-    }
+    chainNumbersTwo += inputNumber;
     return chainNumbersTwo;
 }
 
@@ -120,35 +107,38 @@ function storeNumber2(inputNumber) {
 // function clearFunction(target) {
 //     if (target) {
 //         displayScreen.textContent = '0';
-//         outerClear = 'clear';
-//         return outerClear;
+//         Clear = 'clear';
+//         return Clear;
 //     }
 // }
 
 
 
 
+function addition() {
+    const x = getNumberOne;
+    const y = getNumberTwo;
+    const z = Number(x) + Number(y);
+    return z;
+}
 
+function substraction() {
+    const x = getNumberOne;
+    const y = getNumberTwo;
+    const z = Number(x) - Number(y);
+    return z;   
+}
 
+function multiplication() {
+    const x = getNumberOne;
+    const y = getNumberTwo;
+    const z = Number(x) * Number(y);
+    return z;    
+}
 
-
-// function substraction() {
-//     const x = prompt('input number');
-//     const y = prompt('input number 2');
-//     const z = Number(x) - Number(y);
-//     return z;   
-// }
-
-// function multiplication() {
-//     const x = prompt('input number');
-//     const y = prompt('input number 2');
-//     const z = Number(x) * Number(y);
-//     return z;    
-// }
-
-// function division(){
-//     const x = prompt('input number');
-//     const y = prompt('input number 2');
-//     const z = Number(x) / Number(y);
-//     return z;    
-// }
+function division(){
+    const x = getNumberOne;
+    const y = getNumberTwo;
+    const z = Number(x) / Number(y);
+    return z;    
+}
